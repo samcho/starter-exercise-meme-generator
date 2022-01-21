4389ff13736124905ad2dfd1df4150c7009f2fd6
Samuel Cho <choss@wfu.edu>
2022-01-21 00:18:50 -0500

    Added comments

--- src/MemeGenerator.java
+++ src/MemeGenerator.java
@@ -1,6 +1,11 @@
 public class MemeGenerator {
 
     public static void main(String[] args) {
+        // TODO: Check for command line arguments
+
+        // TODO: Iterate over command line arguments and print out to screen
+
+        // print out Pikachu meme
         System.out.println("⣿⣿⣿⣿⣿⡏⠉⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿");
         System.out.println("⣿⣿⣿⣿⣿⣿⠀⠀⠀⠈⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⠉⠁⠀⣿");
         System.out.println("⣿⣿⣿⣿⣿⣿⣧⡀⠀⠀⠀⠀⠙⠿⠿⠿⠻⠿⠿⠟⠿⠛⠉⠀⠀⠀⠀⠀⣸⣿");
